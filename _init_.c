a7e152fde0655db21d593e72c3e2a50ccf2da094
GreenDAS <greendas8@gmail.com>
2025-10-31 16:01:05 -0400

    Refactor initialization in _init_ function: set up VoltReader and Switch1, configure ADC settings, and setup Timer2, Timer3, & Display objs.

--- _init_.c
+++ _init_.c
@@ -72,7 +72,8 @@ void InitSysTick(int load){
 // Main
 //------------------------------------------------------------------------------
 
-extern IODevice FreqReader;
+extern IODevice VoltReader;
+IODevice Switch1;
 extern GeneralPurposeTimer Timer2;
 GeneralPurposeTimer Timer3;
 extern GenevaLCDDevice Display;
@@ -80,19 +81,25 @@ extern GenevaLCDDevice Display;
 extern const int size;
 
 void _init_(){
-	FreqReader = IODevice_Create('A',0,0,1,'F');
-	FreqReader.initInterupt(FreqReader.pin,FreqReader.GPIOchar,EXTI0_IRQn,1);
-	
-	Timer2 = GeneralPurposeTimer_Create(2,0,0,(4000000000UL),'D',0); // Clock with 1/2 seconds counter
-	Timer2.InteruptHandler = PeripheralInteruptHandling_Create(TIM2_IRQn);
-	//Timer2.TIMX->DIER |= TIM_DIER_UIE; // Enables TIM2's interupt (TIM2's Side)
-	Timer2.InteruptHandler->setPriorityBit(Timer2.InteruptHandler,1); // Sets the Priority Bit
-	Timer2.InteruptHandler->setIXER(Timer2.InteruptHandler,'S'); // Enables the interupt in the NVIC
-	Timer2.InteruptHandler->initCCInterupt(Timer2.TIMX);
-	Timer2.setBits(&(Timer2.TIMX->CR1),0,1);
-	
-	Timer3 = GeneralPurposeTimer_Create(3,1,3999,10000,'D',0); // 1 count is 1ms
-	Display = GenevaLCDDevice_Create(&Timer3,5,10); // Stays on for 500ms off for 2ms
-	
-//	str = StringClass_Create("\0",16);
+	Switch1 = IODevice_Create('C', SW1, 1, 0, 'I'); // Sets up Switch1 (PC4)
+
+	Timer2 = GeneralPurposeTimer_Create(2,1,CountAtMilSecondRate,TimerPeriod1SecondInMilSeconds*10,'D',0); // Sets up Timer2
+	Timer3 = GeneralPurposeTimer_Create(3,1,CountAtMilSecondRate,TimerPeriod1SecondInMilSeconds*10,'D',0); // Sets up Timer3
+	Display = GenevaLCDDevice_Create(&Timer2, 5, 10); // Sets up LCD Display
+
+	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN; // Enable ADC Clock
+	if ((ADC1->CR & ADC_CR_DEEPPWD) == ADC_CR_DEEPPWD){ADC1->CR &= ~ADC_CR_DEEPPWD;} // Wake up ADC from Deep Power Down
+	ADC1->CR |= ADC_CR_ADVREGEN; // Enable ADC Voltage Regulator
+	Timer2.greedyWait(&Timer2, 1, MilSecondsScalar); // Wait for ADC Voltage Regulator to start up (min 10us)
+	VoltReader = IODevice_Create('A', 0, 1, 0, 'A'); // Sets up VoltReader ADC Pin (PA0)
+	VoltReader.GPIOX->ASCR |= (1UL); // Enable Analog Switch for PA0
+	ADC1->SQR1 |= 0x140UL; // Set ADC to use channel 0 (PA0) as 1st conversion
+	ADC123_COMMON->CCR &= ~(0xFUL); // Divide by 1 for ADC clock
+	ADC123_COMMON->CCR &= ~(0x30000UL); // Setup the ADC1 timer so that it will take the HCLK w/ a divide by 1
+	ADC123_COMMON->CCR |= (0x10000UL);
+	ADC1->CFGR |= (0x2UL<<3); // Set ADC to 8 bit resolution
+	ADC1->SMPR1 |= (1<<15); // Set ADC Channel 0 Sample time to 6.5 cycles
+	ADC1->ISR |= ADC_ISR_ADRDY; // Clear ADC Ready flag
+	ADC1->CR |= ADC_CR_ADEN; // Enable ADC
+	while((ADC1->ISR & ADC_ISR_ADRDY) == 0){} // Wait for ADC to be ready
 }
