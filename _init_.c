f25edbeb261e46416512a9fc649eba752ca4c53d
GreenDAS <greendas8@gmail.com>
2025-11-16 19:45:58 -0500

    Updated to 12bits of resolution

--- _init_.c
+++ _init_.c
@@ -129,7 +129,7 @@ void _init_(){
 	ADC123_COMMON->CCR &= ~(0xFUL); // Divide by 1 for ADC clock
 	ADC123_COMMON->CCR &= ~(0x30000UL); // Setup the ADC1 timer so that it will take the HCLK w/ a divide by 1
 	ADC123_COMMON->CCR |= (0x10000UL);
-	ADC1->CFGR |= (0x2UL<<3); // Set ADC to 8 bit resolution
+	ADC1->CFGR |= (0x0UL<<3); // Set ADC to 12 bit resolution
 	ADC1->SMPR1 |= (7UL<<15); // Set ADC Channel 0 Sample time to 6.5 cycles
 	ADC1->ISR |= ADC_ISR_ADRDY; // Clear ADC Ready flag
 	ADC1->CR |= ADC_CR_ADEN; // Enable ADC
