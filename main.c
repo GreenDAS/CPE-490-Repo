a02e4633d88f4827bc02b60e3151176a3181219b
GreenDAS <greendas8@gmail.com>
2025-11-26 15:01:39 -0500

    Updated logic in handlePadPressReady fn to use a ? opp instead of an if statment

--- main.c
+++ main.c
@@ -179,10 +179,7 @@ int voltCalcReady(){return calcVoltFlag;}
 int freqCalcReady(){return calcFreqFlag;}
 int dispUpdaReady(){return 1;}
 int readPadReady(){return gettingUserInputFlag;}
-int handlePadPressReady(){
-	if ((!(NumberPad->state)) == NumberPad->prevState){return 1;}
-	return 0;
-}
+int handlePadPressReady(){return ((!(NumberPad->state)) == NumberPad->prevState) ? 1 : 0;}
 
 // Cooldown Fns
 
