2a6dd04a943d789822753fa004b6f1fdcfb1bd8a
GreenDAS <greendas8@gmail.com>
2025-12-05 10:54:54 -0500

    Increaded # of tasks

--- globals.h
+++ globals.h
@@ -45,7 +45,7 @@
     //
 
 // Scheduler Config
-    #define MAX_TASKS           10                  // Maximum number of tasks for EDF Scheduler
+    #define MAX_TASKS           11                  // Maximum number of tasks for EDF Scheduler
     #define SYSTICK_MAX         1200                // SysTick max value ( Doesn't need to change unless a deadline is longer than this )
     // Deadlines
         #define VOLTAGE_DEADLINE    400             // 400ms
